6be0453a867b3b2898e3a856f34c3ae2941999c6
Yaroslav Ivanov <151339709+kevsinn@users.noreply.github.com>
2023-12-23 11:07:50 +0300

    Delete TownClicker directory

--- TownClicker/Intermediate/Build/Android/a/MyProject/Shipping/Engine/SharedPCH.Engine.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "F:/GAME DEV STUFF/UnrealProjects/TownClicker/Intermediate/Build/Android/a/MyProject/Shipping/Engine/SharedDefinitions.Engine.Cpp20.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
